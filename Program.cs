9233acf0ae9d6eb5fb37a1b183d9c868b315e587
Seiliaz <seiliaz@yahoo.com>
2023-12-23 21:01:15 +0330

    fixed the problems with dependancy inversion and dependency injection

--- Program.cs
+++ Program.cs
@@ -1,38 +1,41 @@
-﻿var cookiesRecipesApp = new CookieRecipesApp();
+﻿var cookiesRecipesApp = new CookieRecipesApp(
+    new RecipesRepository(),
+    new RecipesConsoleUserInteraction()
+);
 cookiesRecipesApp.Run();
 
 public class CookieRecipesApp
 {
-    private readonly RecipesRepository _recipesRepository = new();
-    private readonly RecipesConsoleUserInteraction _recipesConsoleUserInteraction = new();
+    private readonly IRecipesRepository _recipesRepository;
+    private readonly IRecipesUserInteraction _recipesUserInteraction;
 
-    // public CookieRecipesApp(
-    //     RecipesRepository recipesRepository,
-    //     RecipesConsoleUserInteraction recipesConsoleUserInteraction
-    // )
-    // {
-    //     _recipesRepository = recipesRepository;
-    //     _recipesConsoleUserInteraction = recipesConsoleUserInteraction;
-    // }
+    public CookieRecipesApp(
+        IRecipesRepository recipesRepository,
+        IRecipesUserInteraction recipesConsoleUserInteraction
+    )
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesConsoleUserInteraction;
+    }
 
     public void Run()
     {
         var allRecipes = _recipesRepository.Read("filePath");
-        _recipesConsoleUserInteraction.PrintExistingRecipes(allRecipes);
-        _recipesConsoleUserInteraction.PromptToCreateRecipe();
-        var ingredients = _recipesConsoleUserInteraction.ReadIngredientsFromUser();
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+        _recipesUserInteraction.PromptToCreateRecipe();
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
         if (ingredients.Count > 0)
         {
             var recipes = new Recipe(ingredients);
             allRecipes.Add(recipe);
             _recipesRepository.Write("filePath", allRecipes);
-            _recipesConsoleUserInteraction.ShowMessage("Recipe added:");
-            _recipesConsoleUserInteraction.ShowMessage(recipe.toString());
-            _recipesConsoleUserInteraction.Exit();
+            _recipesUserInteraction.ShowMessage("Recipe added:");
+            _recipesUserInteraction.ShowMessage(recipe.toString());
+            _recipesUserInteraction.Exit();
         }
         else
         {
-            _recipesConsoleUserInteraction.ShowMessage(
+            _recipesUserInteraction.ShowMessage(
                 "No ingredients have been selected." +
                 "Recipe will not be saved!"
             );
@@ -40,7 +43,13 @@ public class CookieRecipesApp
     }
 }
 
-public class RecipesRepository
+public interface IRecipesRepository
+{
+    string Read(string filePath);
+    void Write(string filePath, string text);
+}
+
+public class RecipesRepository : IRecipesRepository
 {
     public string Read(string filePath) => filePath;
     public void Write(string filePath, string text)
@@ -49,7 +58,16 @@ public class RecipesRepository
     }
 }
 
-public class RecipesConsoleUserInteraction
+public interface IRecipesUserInteraction
+{
+    void PrintExistingRecipes(string data);
+    void PromptToCreateRecipe();
+    List<string> ReadIngredientsFromUser();
+    void ShowMessage(string message);
+    void Exit();
+}
+
+public class RecipesConsoleUserInteraction : IRecipesUserInteraction
 {
     public void PrintExistingRecipes(string data)
     {
