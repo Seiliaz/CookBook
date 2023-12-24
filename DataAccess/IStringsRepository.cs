e2f9f15f197db114e6b4e7a1e7777dfea933127b
Seiliaz <seiliaz@yahoo.com>
2023-12-24 20:11:21 +0330

    transfered some specific classes to data access folder

---
+++ DataAccess/IStringsRepository.cs
@@ -0,0 +1,11 @@
+using CookBook.Recipes;
+
+namespace CookBook.DataAccess
+{
+    public interface IStringsRepository
+    {
+        List<string> Read(string filePath);
+        void Write(string filePath, List<string> strings);
+    }
+
+}
